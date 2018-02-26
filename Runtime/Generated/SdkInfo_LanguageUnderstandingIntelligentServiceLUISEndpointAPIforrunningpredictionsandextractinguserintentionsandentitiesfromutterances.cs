6c606dc6eafbb6a2a0b0802fdd87bbd68b5c0019
Enzo Cano <enzo.cano@southworks.com>
2018-02-26 18:28:17 -0300

    Add LUIS projects

---
+++ Runtime/Generated/SdkInfo_LanguageUnderstandingIntelligentServiceLUISEndpointAPIforrunningpredictionsandextractinguserintentionsandentitiesfromutterances.cs
@@ -0,0 +1,18 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal static partial class SdkInfo
+{
+    public static IEnumerable<Tuple<string, string, string>> ApiInfo_LanguageUnderstandingIntelligentServiceLUISEndpointAPIforrunningpredictionsandextractinguserintentionsandentitiesfromutterances
+    {
+        get
+        {
+            return new Tuple<string, string, string>[]
+            {
+                new Tuple<string, string, string>("LanguageUnderstandingIntelligentServiceLUISEndpointAPIforrunningpredictionsandextractinguserintentionsandentitiesfromutterances", "Prediction", "v2.0"),
+            }.AsEnumerable();
+        }
+    }
+}
